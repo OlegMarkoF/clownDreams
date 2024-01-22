e267dc593ee61ccb82143fa5d51267169dd4c924
Oleg Markov <o.mark-mail.ru@yandex.ru>
2024-01-22 22:57:30 +0200

    добавлена карусель даты представления

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 // Oбратный отсчет
 const count = document.querySelector(".count");
 // Установите дату, до которой мы ведем обратный отсчет
-let countDownDate = new Date("December 24, 2023 12:00:00").getTime(); // <-- Здесь пишем дату
+let countDownDate = new Date("March 10, 2024 12:00:00").getTime(); // <-- Здесь пишем дату
 let x = setInterval(function () {
   let now = new Date().getTime();
   let distance = countDownDate - now;
@@ -24,6 +24,55 @@ let x = setInterval(function () {
     document.getElementById("t").innerHTML = "Мы готовим новое представление"; // <-- Здесь пишем текст.
   }
 }, 1000);
+
+// карусель даты представлений
+const showTime = document.querySelector("#calendar__slider");
+const showTimeItems = Array.from(showTime.children);
+
+const btnPrev = document.querySelector(".date__prev");
+const btnNext = document.querySelector(".date__next");
+
+showTimeItems.forEach(function (date, index) {
+  
+  // скрываем все слайды кроме первого
+  if (index !== 0) {
+    date.classList.add('hidden')
+  }
+  
+  // добавляем индексы
+  date.dataset.index = index;
+  // добавляем data атрибут active для активного слайда
+  showTimeItems[0].setAttribute('data-active', '');
+})
+
+btnNext.onclick = function () {
+  showNextDate('next');
+}
+
+btnPrev.onclick = function () {
+  showNextDate('prev');
+}
+
+function showNextDate(direction) {
+  // скрыть текущий слайд
+  const currentDate = showTime.querySelector('[data-active]');
+  const currentDateIndex = +currentDate.dataset.index;
+  
+  currentDate.classList.add('hidden');
+  currentDate.removeAttribute('data-active');
+
+  // рассчитываем индекс в зависимости от направления
+  let nextDateIndex;
+  if (direction === 'next') {
+    nextDateIndex = currentDateIndex + 1 === showTimeItems.length ? 0 : currentDateIndex + 1;
+  } else if (direction === 'prev') {
+    nextDateIndex = currentDateIndex === 0 ? showTimeItems.length - 1 : currentDateIndex - 1;
+  }
+  // показать следующий слайд
+  const nextDate = showTime.querySelector(`[data-index="${nextDateIndex}"]`)
+  nextDate.classList.remove('hidden');
+  nextDate.setAttribute('data-active', '');
+}
 
 // логика скрытия хедера - если прогодится.
 let lastScroll = 0;
@@ -151,7 +200,7 @@ function showSlides(n) {
   // dots[slideIndex - 1].className += " active";
 }
 
-// Здесь лежат вызовы функций.
+// Здесь лежат вызовы функций
 openHamburgerButton.addEventListener("click", openHamburger);
 closeHamburgerButton.addEventListener("click", closeHamburger);
 hamburger.addEventListener("click", closeHamburgerOverlay);
